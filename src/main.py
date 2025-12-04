68a19eff34a3439ec9fd160041b108850984f3ee
Tun Kedsaro <tun.k@terradigitalventures.com>
2025-12-04 18:48:07 +0700

    feat(file) : setup initial Docker -> FastAPI -> GCP structure (deploy v.0.0.1)

---
+++ src/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from datetime import datetime, timezone, timedelta
+
+
+class AnalyseRequest(BaseModel):
+    JSON: str | None = None
+
+app = FastAPI(
+    title="CV/Resume Evaluation API",
+    version="0.1.5",
+    description=(
+        "Microservices for CV/Resume evaluation (In progress krub)"
+        "<br>"
+        f"Last time Update : {str(datetime.now(tz=(timezone(timedelta(hours=7)))))}"
+    ),
+    contact={
+        "name": "Tun Kedsaro",
+        "email": "tun.k@terradigitalventures.com"
+    },
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+##############################################################################
+# Health & Metadata
+# 01. Health x / 
+@app.get("/", tags=["Health & Metadata"])
+def health_fastapi():
+    return {"status": "ok", "service": "FastAPI"}
+
+
